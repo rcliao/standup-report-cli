c30fea7d9f76cfa3b7eae747e48d409d96990316
Eric Liao <rcliao01@gmail.com>
2016-12-21 01:20:11 -0800

    feat: parse git-standup format into list

--- main.go
+++ main.go
@@ -5,7 +5,14 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 )
+
+// Report indicate each report entity with commit id and comment
+type Report struct {
+	ID      string
+	Comment string
+}
 
 func main() {
 	name := flag.String("name", "", "Name of the git author")
@@ -26,5 +33,18 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("standup output:\n%s\n", standupOut)
+
+	standupParts := strings.Split(string(standupOut), "\n")
+	commits := []Report{}
+
+	for _, part := range standupParts {
+		commitParts := strings.Split(part, " - ")
+
+		if len(commitParts) == 2 {
+			commitID := commitParts[0]
+			commit := commitParts[1]
+
+			commits = append(commits, Report{commitID, commit})
+		}
+	}
 }
